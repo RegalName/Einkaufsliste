c8190b27a4db0bcad5891b09998f931a5896181c
Gabriel Pospischil <gabriel-pospischil@t-online.de>
2025-05-21 12:49:30 +0200

    can now delete shopping list items

--- app/src/main/java/com/example/einkaufsliste/MainActivity.kt
+++ app/src/main/java/com/example/einkaufsliste/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Adapter
 import android.widget.ArrayAdapter
 import android.widget.EditText
 import android.widget.ListView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.einkaufsliste.databinding.ActivityMainBinding
@@ -32,17 +33,29 @@ class MainActivity : AppCompatActivity() {
         itemAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, einkaeufe)
         lvListe.adapter = itemAdapter
 
+        lvListe.setOnItemLongClickListener({arg0, arg1, pos, id ->
+            einkaeufe.removeAt(pos)
+            itemAdapter.notifyDataSetChanged()
+            Toast.makeText(applicationContext, "Einkauf gelöscht", Toast.LENGTH_SHORT).show()
+            true
+        })
+
         fab.setOnClickListener {
             var builder = AlertDialog.Builder(this)
             builder.setTitle("Hinzufügen")
 
             var input = EditText(this)
-            input.hint = "Text eingeben"
+            input.hint = "Einkauf eingeben"
             input.inputType = InputType.TYPE_CLASS_TEXT
             builder.setView(input)
 
             builder.setPositiveButton("OK") { dialog, which ->
                 einkaeufe.add(input.text.toString())
+            }
+
+            builder.setNegativeButton("Abbrechen") { dialog, which ->
+                Toast.makeText(applicationContext, "Abgebrochen", Toast.LENGTH_SHORT).show()
+
             }
 
             builder.show()
