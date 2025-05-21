4ce1260ff5163419c9c862eb357e204a1e846d67
Gabriel Pospischil <gabriel-pospischil@t-online.de>
2025-05-21 12:21:05 +0200

    Initial commit

---
+++ app/src/main/java/com/example/einkaufsliste/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.einkaufsliste
+
+import android.os.Bundle
+import android.text.InputType
+import android.widget.Adapter
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.ListView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.example.einkaufsliste.databinding.ActivityMainBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var lvListe: ListView
+    private lateinit var fab: FloatingActionButton
+    private lateinit var einkaeufe: ArrayList<String>
+    private lateinit var itemAdapter: ArrayAdapter<String>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        lvListe = findViewById(R.id.lvListe)
+        fab = findViewById(R.id.fabAddElement)
+        einkaeufe = ArrayList()
+
+        itemAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, einkaeufe)
+        lvListe.adapter = itemAdapter
+
+        fab.setOnClickListener {
+            var builder = AlertDialog.Builder(this)
+            builder.setTitle("Hinzufügen")
+
+            var input = EditText(this)
+            input.hint = "Text eingeben"
+            input.inputType = InputType.TYPE_CLASS_TEXT
+            builder.setView(input)
+
+            builder.setPositiveButton("OK") { dialog, which ->
+                einkaeufe.add(input.text.toString())
+            }
+
+            builder.show()
+        }
+
+    }
+
+}
